9e0e7fa9722907fad8d927e10a36887dfe6de872
Takuya Ishibashi <takuya-i@users.sourceforge.net>
2024-05-16 16:49:32 +0900

    add test-sqlite3-large.py test-SQLMap.py

---
+++ test-SQLMap.py
@@ -0,0 +1,149 @@
+import sqlite3
+import resource
+import pickle
+
+# メモリ使用量を 512MB に制限
+max_memory = 512 * 1024 * 1024
+resource.setrlimit(resource.RLIMIT_AS, (max_memory, max_memory))
+
+# データベースに接続（存在しない場合は作成）
+conn = sqlite3.connect('test-sqlite3.db')
+c = conn.cursor()
+
+# ジャーナルモードをWALに設定
+c.execute('PRAGMA journal_mode = WAL')
+# 自動コミットを無効にする
+c.execute('PRAGMA synchronous = OFF')
+# 64MBのキャッシュサイズ (単位: ページ, デフォルトは1024バイト/ページ)
+c.execute('PRAGMA cache_size = -64000')
+
+class SQLMap():
+    def __init__(self, table_name):
+        self.table_name = table_name
+        c.execute('''
+        CREATE TABLE IF NOT EXISTS {} (
+        key TEXT PRIMARY KEY,
+        value TEXT
+        )
+        '''.format(table_name))
+
+    def put(self, key, value, commit=False):
+        serialized_data = pickle.dumps(value)
+        print(len(serialized_data))
+        c.execute('''
+        INSERT OR REPLACE INTO {} (key, value)
+        VALUES (?, ?)
+        '''.format(self.table_name), (key, serialized_data))
+        if commit:
+            conn.commit()
+
+    def get(self, key):
+        result = c.execute('''
+        SELECT value FROM {} WHERE key = ?
+        '''.format(self.table_name), (key,))
+        row = result.fetchone()
+        return pickle.loads(row[0]) if row else None
+
+    # sort: None, 'ASC', 'DESC'
+    def iterator(self, sort=None, offset=0, limit=-1):
+        sql = f'SELECT value FROM {self.table_name}'
+        if sort is not None:
+            if sort.upper() == 'ASC':
+                sql += ' ORDER BY key ASC'
+            elif sort.upper() == 'DESC':
+                sql += ' ORDER BY key DESC'
+        sql += f' LIMIT {limit} OFFSET {offset}'
+        print(f'sql={sql}')
+        result = c.execute(sql)
+        while True:
+            row = result.fetchone()
+            if row is None:
+                break
+            yield pickle.loads(row[0])
+
+
+class SQLList():
+    def __init__(self, table_name):
+        self.table_name = table_name
+        c.execute('''
+        CREATE TABLE IF NOT EXISTS {} (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        data BLOB
+        )
+        '''.format(table_name))
+
+    def insert(self, data, commit=False):
+        serialized_data = pickle.dumps(data)
+        c.execute('''
+        INSERT INTO {} (data) VALUES (?)
+        '''.format(self.table_name), (serialized_data,))
+        if commit:
+            conn.commit()
+
+    def get(self, data_id):
+        result = c.execute('SELECT data FROM {} WHERE id = ?'.format(self.table_name), (data_id,))
+        row = result.fetchone()
+        return pickle.loads(row[0]) if row else None
+
+    # sort: None, 'ASC', 'DESC'
+    def iterator(self, sort=None, offset=0, limit=-1):
+        sql = f'SELECT data FROM {self.table_name}'
+        if sort is not None:
+            if sort.upper() == 'ASC':
+                sql += ' ORDER BY id ASC'
+            elif sort.upper() == 'DESC':
+                sql += ' ORDER BY id DESC'
+        sql += f' LIMIT {limit} OFFSET {offset}'
+        print(f'sql={sql}')
+        result = c.execute(sql)
+        while True:
+            row = result.fetchone()
+            if row is None:
+                break
+            yield pickle.loads(row[0])
+
+
+class Entry():
+    TYPE_FILE = 'F'
+    TYPE_DIR = 'D'
+    TYPE_SYMLINK = 'S'
+    TYPE_OTHER = 'O'
+
+    def __init__(self, path, mode, file_type, uname, gname,
+                 size, mtime, linkname):
+        self.path = path
+        self.mode = mode
+        self.file_type = file_type
+        self.uname = uname
+        self.gname = gname
+        self.size = size
+        self.mtime = mtime
+        self.linkname = linkname
+
+    def __repr__(self):
+        return f'Entry(path={self.path},mode={self.mode})'
+
+
+def main():
+    el = SQLList('entrylist')
+    ent1 = Entry('abc1', 0o777, Entry.TYPE_FILE, 'user1', 'group1', 0, 100, None)
+    ent2 = Entry('abc2', 0o777, Entry.TYPE_FILE, 'user1', 'group1', 0, 100, None)
+    ent3 = Entry('abc3', 0o777, Entry.TYPE_FILE, 'user1', 'group1', 0, 100, None)
+    el.insert(ent1)
+    el.insert(ent2)
+    el.insert(ent3)
+    ent = el.get(2)
+    print(ent)
+    for ent in el.iterator(offset=1, sort='DESC'):
+        print('???: ' + str(ent))
+
+    em = SQLMap('entrymap')
+    em.put(ent1.path, ent1)
+    em.put(ent2.path, ent2)
+    em.put(ent3.path, ent3)
+    ent = em.get(ent2.path)
+    print(ent)
+    for ent in em.iterator(offset=1, sort='DESC'):
+        print('???: ' + str(ent))
+
+main()
---
+++ test-sqlite3-large.py
@@ -0,0 +1,153 @@
+import os
+import sqlite3
+import random
+import string
+import time
+import sys
+import resource
+
+max_memory = 512 * 1024 * 1024
+resource.setrlimit(resource.RLIMIT_AS, (max_memory, max_memory))
+
+# データベースファイルのパス
+db_file = 'test-sqlite.db'
+
+# データベースファイルが存在する場合は削除
+if os.path.exists(db_file):
+    os.remove(db_file)
+
+# SQLiteデータベースの作成と接続
+conn = sqlite3.connect(db_file)
+#conn = sqlite3.connect(':memory:')
+c = conn.cursor()
+
+# テーブルの作成
+c.execute('''CREATE TABLE data (path TEXT, mode INTEGER, type TEXT, user TEXT, gname TEXT, size INTEGER, mtime INTEGER, linkname TEXT)''')
+c.execute('PRAGMA synchronous = OFF')  # 効果は小さい
+#c.execute('CREATE INDEX idx_path ON data (path)')  # INDEX 作成しながらだと効率悪い
+
+# データの挿入
+total_entries = 100000000  # 1億エントリ
+#total_entries =  10000000 # 1000万
+#total_entries =  1000000 # 100万
+
+batch_size = 1000000  # バッチサイズ
+#batch_size = 10000  # バッチサイズ
+
+start_time = time.time()
+previous = start_time
+
+for i in range(total_entries, 0, -batch_size): # 降順
+    data = []
+    now = time.time()
+    print(f'progress: {i}/{total_entries} | {now - previous} | {now - start_time}')
+    previous = now
+    for j in range(i, i + batch_size):
+        # path, mode, file_type(str), uname, gname, size, mtime, linkname(str)
+        data.append([hex(j) * 10, 0o600, 'FILE', 'user001122', 'user001122', 1000, 1707405506, ""])
+    c.executemany('INSERT INTO data VALUES (?, ?, ?, ?, ?, ?, ?, ?)', data)
+
+conn.commit()
+
+insertion_time = time.time() - start_time
+print(f'Data insertion took {insertion_time} seconds')
+
+start_time = time.time()
+c.execute('CREATE INDEX idx_path ON data (path)') # あとから INDEX 作成
+index_time = time.time() - start_time
+print(f'Data index creation took {index_time} seconds')
+
+# ソートされたデータの取得と表示
+#start_time = time.time()
+
+# ソート済みの一時テーブルを作成
+#c.execute("CREATE TEMPORARY TABLE sorted_table AS SELECT * FROM data ORDER BY path DESC")
+
+#sort_time = time.time() - start_time
+#print(f'Data pre sorting took {sort_time} seconds')
+
+# グループごとに分けて取得
+start_time = time.time()
+
+# テーブルの全体数を取得
+c.execute("SELECT COUNT(*) FROM data")
+total_rows = c.fetchone()[0]
+
+if total_rows != total_entries:
+    print(f'total_rows{total_rows} != total_entries{total_entries}')
+    sys.exit(1)
+
+# 各グループのサイズを計算
+group_num = 4
+group_size = total_entries / group_num
+remainder = total_entries % group_num
+
+# グループごとの範囲を定義
+ranges = []
+start_index = 0
+for i in range(group_num):
+    end_index = start_index + group_size + (1 if i < remainder else 0)
+    ranges.append((start_index, end_index))
+    start_index = end_index
+
+#sorted_data = c.execute('SELECT * FROM data ORDER BY path DESC') # 降順
+#sorted_data = c.execute('SELECT * FROM data ORDER BY path ASC') # 昇順
+
+group_sql = []
+for start, end in ranges:
+    # LIMIT offset, count
+    count = end - start
+    sorted_sql = f'SELECT * FROM data ORDER BY path DESC LIMIT {start}, {count}'
+    group_sql.append(sorted_sql)
+
+sort2_time = time.time() - start_time
+print(f'Data sorting(prepare) took {sort2_time} seconds')
+
+# 最初の3行を表示
+start_time = time.time()
+for g in group_sql:
+    print("-------------------------------------------")
+    result = c.execute(g)
+    for row in result.fetchmany(3):
+        print(row)
+
+fetch_time = time.time() - start_time
+print(f'Data fetching(with sorting) (1) took {fetch_time} seconds')
+
+# 最初の3行を表示 2回目
+# start_time = time.time()
+# for g in group_sql:
+#     print("-------------------------------------------")
+#     result = c.execute(g)
+#     for row in result.fetchmany(3):
+#         print(row)
+
+fetch_time = time.time() - start_time
+print(f'Data fetching(with sorting) (2) took {fetch_time} seconds')
+
+print("-------------------------------------------")
+# ジェネレータ関数を定義してデータを1エントリずつ取得する
+def fetch_data(cursor):
+    while True:
+        row = cursor.fetchone()
+        if row is None:
+            break
+        yield row
+
+start_time = time.time()
+for g in group_sql:
+    result = c.execute(g)
+    for row in fetch_data(result):
+        #print(row)
+        pass
+fetch_time = time.time() - start_time
+print(f'Data fetching all took {fetch_time} seconds')
+
+# データベースファイルのサイズを取得
+db_size = os.path.getsize(db_file)
+
+# データベースファイルのサイズを表示
+print(f'Database Size: {db_size} bytes')
+
+# 接続のクローズ
+conn.close()
